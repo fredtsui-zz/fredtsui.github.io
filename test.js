6080cefa559e77f6ff00a079af61d05e4e465472
fredtsui <yizhi.tsui@gmail.com>
2018-09-25 08:45:47 -0400

    update for winter 2019

---
+++ test.js
@@ -0,0 +1,13 @@
+var url = "http://localhost:8080";
+
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+
+var ret = httpGet(url);
+alert(ret);
